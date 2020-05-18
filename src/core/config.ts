483dec1f6b21a3500fbec4519f1ba59c219c9021
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2020-05-18 22:11:32 +0300

    fix: removed left side resize markers

--- src/core/config.ts
+++ src/core/config.ts
@@ -1,5 +1,5 @@
 export const APP_PREFIX = 'investor-desktop-'
-export const PIN_POSITIONS: Array<string> = ['right', 'bottom-right', 'bottom-middle', 'bottom-left', 'left']
+export const PIN_POSITIONS: Array<string> = ['right', 'bottom-right', 'bottom-middle']
 export const WS_API_URL = 'wss://ws.blockchain.info/inv'
 export const SUBSCRIBE = { op: 'unconfirmed_sub' }
 export const UNSUBSCRIBE = { op: 'unconfirmed_unsub' }
